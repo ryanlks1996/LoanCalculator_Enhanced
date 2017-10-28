10f249f70475417f4a6b71145a8e2f29487cd61b
Ryan <leongks-wa15@student.tarc.edu.my>
2017-10-28 10:04:07 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/loancalculator/MainActivity.java
@@ -0,0 +1,59 @@
+package my.edu.tarc.loancalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewDisplay;
+    EditText editTextSalary;
+    EditText editTextPrice;
+    EditText editTextDownPayment;
+    EditText editTextRepayment;
+    EditText editTextInterest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewDisplay = (TextView) findViewById(R.id.textViewDisplay);
+        editTextSalary = (EditText) findViewById(R.id.editTextSalary);
+        editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        editTextDownPayment = (EditText) findViewById(R.id.editTextDownPayment);
+        editTextRepayment = (EditText) findViewById(R.id.editTextRepayment);
+        editTextInterest = (EditText) findViewById(R.id.editTextInterest);
+
+    }
+
+    public void checkLoanStatus(View v){
+        double interest =  Double.parseDouble(editTextInterest.getText().toString());
+        int salary = Integer.parseInt(editTextSalary.getText().toString());
+        int price = Integer.parseInt(editTextPrice.getText().toString());
+        int downPayment = Integer.parseInt(editTextDownPayment.getText().toString());
+        int repayment = Integer.parseInt(editTextRepayment.getText().toString());
+        int interestRate = Integer.parseInt(editTextInterest.getText().toString());
+
+        double totalInterest = (price-downPayment) * interestRate * ((double)repayment);
+        double totalLoan = (price-downPayment)+totalInterest;
+        double monthPayment = totalLoan / interestRate;
+
+        if(monthPayment > (salary*0.3))
+            textViewDisplay.setText("Status: Reject");
+        else
+            textViewDisplay.setText("Status: Accept");
+
+    }
+    public void clearText(View v){
+
+        textViewDisplay.setText(" ");
+        editTextSalary.setText(" ");
+        editTextPrice.setText(" ");
+        editTextDownPayment.setText("");
+        editTextRepayment.setText("");
+        editTextInterest.setText("");
+
+    }
+}
